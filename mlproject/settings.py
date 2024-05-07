07878fb68a554a4b5db76d43e49c161c98c58393
Kanishk Nagpal <kaynagpal03@gmail.com>
2024-05-08 05:23:19 +0530

    fix deployment

--- mlproject/settings.py
+++ mlproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-n3se5fyd3&zf+)!3y0n55!r(+5_qne)mwe-dd4o6_d33goysg7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*', '.vercel.app']
+ALLOWED_HOSTS = ['*', '.vercel.app', '.now.sh']
 
 
 # Application definition
@@ -116,9 +116,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 STATIC_URL = 'static/'
 
 
