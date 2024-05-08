00e4ba073bb12cc151ae0bd2e91bf0faccd50737
Kanishk Nagpal <kaynagpal03@gmail.com>
2024-05-08 06:11:30 +0530

    fixed function

--- build_files.sh
+++ build_files.sh
@@ -2,8 +2,8 @@ echo "Building the project..."
 
 PYTHON_VERSION="3.9"
 # apt update
-# apt install -y python${PYTHON_VERSION} python${PYTHON_VERSION}-dev python${PYTHON_VERSION}-distutils
-# curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-# python${PYTHON_VERSION} get-pip.py
+apt install -y python${PYTHON_VERSION} python${PYTHON_VERSION}-dev python${PYTHON_VERSION}-distutils
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python${PYTHON_VERSION} get-pip.py
 python3.9 -m pip install -r requirements.txt
 python3.9 manage.py collectstatic
