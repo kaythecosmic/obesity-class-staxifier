ba393f61dbb1fabebaa77059bee4ee295e4a3a4e
Kanishk Nagpal <kaynagpal03@gmail.com>
2024-05-08 06:04:48 +0530

    filler

--- build_files.sh
+++ build_files.sh
@@ -1,8 +1,9 @@
 echo "Building the project..."
 
+PYTHON_VERSION="3.9"
+apt update
+apt install -y python${PYTHON_VERSION} python${PYTHON_VERSION}-dev python${PYTHON_VERSION}-distutils
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python3.9 get-pip.py
-chmod +x install_pip.sh
-./install_pip.sh
+python${PYTHON_VERSION} get-pip.py
 python3.9 -m pip install -r requirements.txt
 python3.9 manage.py collectstatic
