74ba16984c8212076f97a5de82bee48f990fcee4
Kanishk Nagpal <kaynagpal03@gmail.com>
2024-05-08 05:59:52 +0530

    filler

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,7 @@
 echo "Building the project..."
-python3.9 install -r requirements.txt
+
+python3.9 --version
+python3.9 -m pip --version
+cat requirements.txt
+python3.9 -m pip install -r requirements.txt
 python3.9 manage.py collectstatic
